dca1936c9c6414e518891655a3b0fa107c1dfb6f
Charly <cgtright@gmail.com>
2020-03-03 16:26:57 -0600

    Terminada la APP

--- frontend/src/components/mapbox/map1.js
+++ frontend/src/components/mapbox/map1.js
@@ -5,9 +5,9 @@ import mapboxgl from 'mapbox-gl'
 
   class MapEmergency extends Component {
     state = {
-      lng: -99.1635,
-      lat: 19.4215,
-      zoom: 15
+      lng: 5,
+      lat: 34,
+      zoom: 2
     }
     
     componentDidMount() {
@@ -44,7 +44,58 @@ import mapboxgl from 'mapbox-gl'
           .setLngLat([-99.1632, 19.4215])
           .setPopup(popup)
           .addTo(map)
+
+          const popup1 = new mapboxgl.Popup({ offset: 25 }).setText(
+            'Hospital Veterinario Durango: Calle de Durango 127, Roma Nte., Cuauhtémoc, 06700 Ciudad de México, CDMX'
+            );
+            
+            const el1 = document.createElement('div');
+            el1.id = 'marker1';
+      
+          new mapboxgl.Marker(el1)
+          .setLngLat([-99.1624, 19.4208])
+          .setPopup(popup1)
+          .addTo(map)
+
+          const popup2 = new mapboxgl.Popup({ offset: 25 }).setText(
+            'Cambia un destino hospital veterinario: Av Chapultepec 238, Roma Nte., Cuauhtémoc, 06700 Ciudad de México, CDMX'
+            );
+            
+            const el2 = document.createElement('div');
+            el2.id = 'marker2';
+      
+          new mapboxgl.Marker(el2)
+          .setLngLat([-99.1621, 19.4188])
+          .setPopup(popup2)
+          .addTo(map)
+
+          const popup3 = new mapboxgl.Popup({ offset: 25 }).setText(
+            'Calle Cruz Valle Verde 34, Santa Cruz del Monte, Cuauhtémoc, 06700 Naucalpan de Juárez, CDMX'
+            
+            );
+            
+            const el3 = document.createElement('div');
+            el3.id = 'marker3';
+      
+          new mapboxgl.Marker(el3)
+          .setLngLat([-99.1595, 19.4244])
+          .setPopup(popup3)
+          .addTo(map)
+      
+          const popup4 = new mapboxgl.Popup({ offset: 25 }).setText(
+            'Centro veterinario Bichos: Valladolid 34, Roma Nte., Cuauhtémoc, 06700 Ciudad de México, CDMX'
+            
+            );
+            
+            const el4 = document.createElement('div');
+            el4.id = 'marker4';
+      
+          new mapboxgl.Marker(el4)
+          .setLngLat([-99.1684, 19.4210])
+          .setPopup(popup4)
+          .addTo(map)
     }
+
   
 
   
--- frontend/src/pages/emergency.js
+++ frontend/src/pages/emergency.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import MapEmergency from '../components/mapbox/map1'
-import { Flex } from '@chakra-ui/core'
+import { Flex, Box, Image } from '@chakra-ui/core'
+import customTheme from '../theme'
 
 function Emergency() {
   return (
-    <Flex w="100%" justifyContent="center">
+<Flex w="100%" justifyContent="center">
+   <Box w='95%' h='auto' marginTop="50px" marginBottom="50px" rounded="md" bg={customTheme.themeColors[200]} boxShadow="xl" justifyContent="center">
+    <Flex w="100%" justifyContent="center" flexWrap="wrap" direction="row">
+    <Image w="200px"src="/images/emergencyLogo.svg"></Image>
     <MapEmergency></MapEmergency>
+    <h1>Da click en el Icono de Ubicación en el mapa, para mostrarte los hospitales veterinarios más cercanos a tu Ubicación!</h1>
     </Flex>
+    </Box>
+   </Flex>
    );
 }
 
